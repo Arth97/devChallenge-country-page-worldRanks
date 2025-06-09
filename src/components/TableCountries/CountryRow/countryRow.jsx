bcd1f1ab7274601e3a2326925243d5a2d5af8ecf
Arturo <arturotinajas@gmail.com>
2025-06-09 13:12:10 +0200

    Sort

--- src/components/TableCountries/CountryRow/countryRow.jsx
+++ src/components/TableCountries/CountryRow/countryRow.jsx
@@ -7,8 +7,8 @@ const CountryRow = ({country}) => {
       <tr>
 				<td className="py-2"><img	src={country.flags.svg} alt={country.flags.alt} width={50} height={40} /></td>
 				<td className="py-2 max-w-250" style={{ maxWidth: '250px' }}>{country?.name?.official}</td>
-				<td className="py-2">{country?.population}</td>
-				<td className="py-2">{country?.area}</td>
+				<td className="py-2">{country?.population?.toLocaleString('es-ES')}</td>
+				<td className="py-2">{country?.area?.toLocaleString('es-ES')}</td>
 				<td className="py-2">{country?.region}</td>
 			</tr>
     </>
--- src/components/TableCountries/tableCountries.jsx
+++ src/components/TableCountries/tableCountries.jsx
@@ -6,8 +6,11 @@ import { TableOptionsContext } from '../../context/TableOptionsContext';
 
 const TableCountries = () => {
 	const [data, setData] = useState(null);
+	const [sortedData, setSortedData] = useState(null);
 
-	const { sort, regions, status  } = useContext(TableOptionsContext);
+	const { sort, regions, status  } = useContext(ContextProvider);
+
+	const sortCache = useRef({})
 
 	useEffect(() => {
 		fetchData()
@@ -18,15 +21,37 @@ const TableCountries = () => {
 			// console.log("sort", sort)
 			// console.log(`https://restcountries.com/v3.1/all?sort=${sort}`)
 			// const response = await fetch(`https://restcountries.com/v3.1/all?sort=${sort}`);
-			const response = await fetch('https://restcountries.com/v3.1/all');
+			// const response = await fetch('https://restcountries.com/v3.1/all');
+			const response = await fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,area,region');
 			const data = await response.json();
 			setData(data)
+			sortCache.current = {};
 			console.log('Fetched data:', data);
 			return data 
 		} catch (err) {
 			console.error('Error fetching data:', err);
 		}
 	}
+
+	useEffect(() => {
+		if (!sortCache.current[sort] && !data) return;
+
+		if (sortCache.current[sort]) {
+			setSortedData(sortCache.current[sort]);
+			return;
+		} else {
+			let sortedData = []
+			if (sort === 'population') {
+        sortedData = [...data].sort((a, b) => b.population - a.population);
+      } else if (sort === 'area') {
+        sortedData = [...data].sort((a, b) => b.area - a.area);
+      } else {
+        sortedData = [...data].sort((a, b) => a.name.common.localeCompare(b.name.common));
+      }
+      sortCache.current[sort] = sortedData;
+			setSortedData(sortedData);
+		}
+	},[data, sort]);
 
   return (
 		<div className="w-2/3">
@@ -41,7 +66,7 @@ const TableCountries = () => {
 					</tr>
 				</thead>
 				<tbody>
-					{data && data.map((country, index) => (
+					{sortedData && sortedData.map((country, index) => (
 						<CountryRow country={country} key={index} />
 					))}
 				</tbody>
